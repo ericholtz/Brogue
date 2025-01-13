206ae0b6cf0881efcb6cdbfab8782512b17e1b88
AlexanderTCSE <tattersallalexd@gmail.com>
2025-01-13 13:54:35 -0800

    Basic grid movement and player animation

---
+++ Scripts/Player.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var PlayerAnim = $AnimatedSprite2D
+
+var tileSize = 16
+var inputs = {"Up": Vector2.UP,
+			"Left": Vector2.LEFT,
+			"Right": Vector2.RIGHT,
+			"Down": Vector2.DOWN}
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tileSize)
+	position += Vector2.ONE * tileSize/2
+	PlayerAnim.play("Idle")
+
+func _input(event):
+	for dir in inputs.keys():
+		if event.is_action_pressed("Left"):
+			PlayerAnim.flip_h = true
+		if event.is_action_pressed("Right"):
+			PlayerAnim.flip_h = false
+		if event.is_action_pressed(dir):
+			move(dir)
+
+func move(dir):
+	position += inputs[dir] * tileSize
