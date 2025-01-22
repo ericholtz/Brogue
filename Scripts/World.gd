c40c8b1ae467490d46d50b45ab6073bb1902e831
AlexanderTCSE <tattersallalexd@gmail.com>
2025-01-21 21:25:01 -0800

    add enemy turn logic, reference to player object for value manipulation

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var turnCounter = 0;
+@onready var player = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,8 +10,18 @@ func _ready():
 #function to take a turn, should basically wait for the player signal then handle all the enemies
 #made it take any value in case we want faster enemies or slower player debuffs
 func takeTurn(turnsTaken: int):
+	print("Starting player turn")
 	turnCounter += turnsTaken
 	print("Current turn: ",turnCounter);
+	#apply over-time effects, increment timers, whatever is appropriate here
+	print("Ending player turn")
+	enemyTurn()
+
+#player and enemy turns are separated out so the player always gets priority over the enemies (unless debuffs change that)
+func enemyTurn():
+	print("Starting enemy turn")
+	#handle enemy signals here
+	print("Ending enemy turn")
 
 #this listens for any input from the player and increments the turn counter
 func _on_player_input_event():
