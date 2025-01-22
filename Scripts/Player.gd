3ea7f67e33776c90a4db087d2e787c84d7ac967b
AlexanderTCSE <tattersallalexd@gmail.com>
2025-01-21 19:14:58 -0800

    implement rudimentary player turn counter

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -40,3 +40,4 @@ func move(dir):
 		moving = true #set this to true until tween is finished to disallow multiple moves at once
 		await tween.finished
 		moving = false
+		emit_signal("input_event") #emit a movement signal here, after the player succesfully moves
