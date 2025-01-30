d6f856edcf06868ac99dc3bdee7ff2568a986015
ericholtz <170209571+ericholtz@users.noreply.github.com>
2025-01-30 02:08:13 -0800

    Refactor the current messy signal structure to direct signals through GM
    improve signal definitions and add item pickup signal
    refactor gain_gold as more generic collectItem for expansion
    use signal.emit rather than emit_signal to remove editor error and improve code cleanliness

--- Core/GameMaster.gd
+++ Core/GameMaster.gd
@@ -1,9 +1,20 @@
 extends Node
 
-signal gained_gold(int)
+signal gained_gold(gold_count: int)
+signal gained_item(item_name: String)
 
 var gold = 0
 
-func gain_gold(gold_gained:int):
-	gold += gold_gained
-	emit_signal("gained_gold", gold_gained)
+func collectItem(itemName: String):
+	match itemName:
+		"SmallGold":
+			gained_gold.emit(2)
+			gold += 2
+		"MediumGold":
+			gained_gold.emit(5)
+			gold += 5
+		"LargeGold":
+			gained_gold.emit(10)
+			gold += 10
+		_:
+			gained_item.emit(itemName)
