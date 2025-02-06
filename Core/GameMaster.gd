7110ccd0e1b63d53999acfb0f403cdd5661b0a9d
martigab000 <martigab000@live.com>
2025-02-06 12:13:34 -0800

    Set up start menu and name text box

--- Core/GameMaster.gd
+++ Core/GameMaster.gd
@@ -2,8 +2,10 @@ extends Node
 
 signal gained_gold(gold_count: int)
 signal gained_item(item_name: String)
+signal set_name(player_name: String)
 
 var gold = 0
+var can_move = false
 
 func collectItem(itemName: String):
 	match itemName:
@@ -18,3 +20,8 @@ func collectItem(itemName: String):
 			gained_gold.emit(10)
 		_:
 			gained_item.emit(itemName)
+
+func setname(player_name: String):
+	if player_name:
+		can_move = true
+		set_name.emit(player_name)
---
+++ Scripts/text_box.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var text_box_container = $TextBoxContainer
+
+@onready var p_name = $TextBoxContainer/MarginContainer/VBoxContainer/HBoxContainer/LineEdit
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+func _hide_text_box(_unused_arg = null):
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_text_submit"):
+		if p_name.text:
+			text_box_container.hide()
+			#load("res://Scenes/world.tscn")
+			
+			
+			GameMaster.setname(p_name.text)
+			#print(p_name.text)
+			#get_tree().change_scene_to_file("res://Scenes/world.tscn")
+		else:
+			print("NO TEXT")
+	pass
