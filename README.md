0a74f87d4b50cb6e51e61d80c3c80f4bff01e828
Microgoober <silasmccloud.com>
2025-02-13 10:48:03 -0800

    I updated the README, dealeated a  few files.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Brogue
-A modern recreation of the original Rogue in Godot.
+# Brogue #
+	## Project information ##
+		Title: Brogue
+		Members: Silas, Gabe, Alex, Eric
+		Description: A modern recreation of the original Rogue.
+		Table of contents: Work in progress
+		Engin: Godot
+	
+	## Rogue ##
+		Here is a link to Wiki witch has more details on the original rouge.
+		(https://en.wikipedia.org/wiki/Rogue_(video_game))
+	
+	## Game Deatails ##
+		Rogue is a single player Roguelike Dungeon Crawler game.
+		You have a 2D grid that the player and all objects sit upon, called a tile_map. 
+		On this grid you have things like walls, monsters, traps, items, and the player.
+		The goal of the game is to get as deep in the dungeon as possible.
+	
+	## Installation ##
+		Yeah... so... Learn Git... And Godot...
+		At this time you have to download Godot first.
+		Then download the git zip for the project into the godot file structure.
+		Then open it in godot and exicute it.
+	
+	## How To Play ##
+		Use W or H to move up
+		Use A or J to move left
+		Use S or K to move Down
+		Use D or L to move Left
+	
+	## Cool Cams ##
+		### Eric ###
+			Each of you add here!
+			Items, gold...
+		
+		### Gabe ###
+			Each of you add here!
+			Map Gen, levels...
+		
+		### Alex ###
+			Each of you add here!
+			Player... GameMaster...
+		
+		### Silas ###
+			A couple different anamations for each enemy Type.
+			A manual Astar Pathing algorithom.
+			More to come...
+	
+	## Contribution Guidelines ##
+		Reach out to us!
+		We are not looking for any outside help at this time.
+		But we will talk to you if you have a good idea!
+
+	## License information ##
+		Apache License
+		Version 2.0, January 2004
+		http://www.apache.org/licenses/
+		More info on the GitHub
--- Scripts/Skeleton_Warrior.gd
+++ Scripts/Skeleton_Warrior.gd
@@ -3,6 +3,9 @@ extends Area2D
 @onready var Animations = $AnimatedSprite2D
 @onready var Ray = $RayCast2D
 
+@onready var monster = "Skeleton Warrior/Area2D"
+@onready var player = "Player"
+
 var animationSpeed = 18 #Used what player was
 var moving = false
 
@@ -10,9 +13,10 @@ var moving = false
 var gold = 0
 
 #Monsters States
-var health = 10
-var strength = 1
-var defense = 1
+var Health = 10
+var Str = 2
+var Def = 1
+var Movement_Speed = 1
 
 var tileSize = 16
 
@@ -32,21 +36,59 @@ func _input(event):
 	if not GameMaster.can_move or moving:
 		#if we're already tweening movement, don't move again
 		return
+		
+	
+	#var rng = RandomNumberGenerator.new()
+	#var my_random_number = rng.randf_range(1, 4)
+	#var dir = Input.get_vector("Left", "Right", "Up", "Down")
+	
 	for dir in inputs.keys():
 		if event.is_action_pressed("Left"):
 			Animations.flip_h = true
+			#This Makes enemy face correct direction
 		if event.is_action_pressed("Right"):
 			Animations.flip_h = false
+			#This Makes enemy face correct direction
 		if event.is_action_pressed(dir):
 			move(dir)
 
-func move(dir):
-	Ray.target_position = inputs[dir] * tileSize	#set ray to move direction +16 pixels
+
+#func move_monster_towards_player(monster: Area2D, player: Area2D) -> void:
+	#var step_size := 16
+	#var monster_pos := monster.global_position
+	#var player_pos := player.global_position
+
+	#var dx := player_pos.x - monster_pos.x
+	#var dy := player_pos.y - monster_pos.y
+
+	#if abs(dx) > abs(dy):
+		# Move horizontally
+		#monster.global_position.x += step_size if dx > 0 else -step_size
+	#else:
+		# Move vertically
+		#monster.global_position.y += step_size if dy > 0 else -step_size
+	
+
+
+
+
+
+
+func move(direction):
+	print(direction)
+	
+	#var min_val = 1
+	#var max_val = 4
+	#var rng = RandomNumberGenerator.new()
+	#var ran_num = rng.randi_range(min_val, max_val)
+	#var dir = str(ran_num)
+	
+	Ray.target_position = inputs[direction] * tileSize	#set ray to move direction +16 pixels
 	Ray.force_raycast_update()
 	if !Ray.is_colliding(): #if ray is colliding with a wall, we can't move there
 		var tween = create_tween() #create a new Tween object to handle smooth movement
 		#tween the position property of self to a position of +16 pixels in the input direction, on a sin curve
-		tween.tween_property(self, "position", position + inputs[dir] * tileSize, 1.0/animationSpeed).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(self, "position", position + inputs[direction] * tileSize, 1.0/animationSpeed).set_trans(Tween.TRANS_SINE)
 		moving = true #set this to true until tween is finished to disallow multiple moves at once
 		await tween.finished
 		moving = false
