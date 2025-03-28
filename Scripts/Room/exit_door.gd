4ef92f6f4294320586b5a27f94842b24069bdf81
ericholtz <170209571+ericholtz@users.noreply.github.com>
2025-03-27 20:44:58 -0700

    move reset zoom and fog functionality to map_gen and add player status toggle to exit door

--- Scripts/Room/exit_door.gd
+++ Scripts/Room/exit_door.gd
@@ -1,12 +1,9 @@
 extends Area2D
 
-@export var entity_type: GameMaster.EntityType
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		# call function to start new level
-		get_node("/root/World/map_gen").regenerate_map()
-		# reset zoom and fog from possible map use
-		GameMaster.DISABLE_FOG = false
-		body.find_child("Camera2D").zoom = Vector2(3, 3)
+		body.is_standing_on_exit = true
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		body.is_standing_on_exit = false
