2a3220ab777ef23f8a2c809fb5885e19cfc33fa0
ericholtz <170209571+ericholtz@users.noreply.github.com>
2025-03-28 22:57:23 -0700

    add scroll item and script

---
+++ Scripts/Items/scroll.gd
@@ -0,0 +1,8 @@
+extends "item.gd"
+
+@export var effect: GameMaster.ScrollEffect
+
+func _ready():
+	effect = GameMaster.ScrollEffect.values().pick_random()
+	entity_name = "Scroll titled '" + GameMaster.scroll_names[effect] + "'"
+	find_child("Sprite2D").set_region_rect(GameMaster.scroll_sprite_regions[effect])
