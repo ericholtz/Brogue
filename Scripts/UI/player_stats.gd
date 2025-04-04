1573908d33158fa303a4709e2216e9c9603a2149
ericholtz <170209571+ericholtz@users.noreply.github.com>
2025-04-03 17:29:08 -0700

    add identify functionality to tab menu
    - add fields for use button and identify button lists
    - start identify button list off as invisible
      (should only see identify button list if we use an identify scroll)
    - add identify functionality and update inventory list creation

--- Scripts/UI/player_stats.gd
+++ Scripts/UI/player_stats.gd
@@ -11,6 +11,8 @@ extends CanvasLayer
 @onready var xp_to_next_label = $PanelContainer/VBoxContainer/StatsBox/GridContainer/XPtoNextVal
 @onready var lvl_label = $PanelContainer/VBoxContainer/StatsBox/GridContainer/LevelVal
 @onready var inventory_list = $PanelContainer/VBoxContainer/Inventory/ItemList
+@onready var use_button_list = $PanelContainer/VBoxContainer/Inventory/ItemList/UseButtons
+@onready var identify_button_list = $PanelContainer/VBoxContainer/Inventory/ItemList/IdentifyButtons
 @onready var resume_button = $PanelContainer/VBoxContainer/HBoxContainer/Resume
 @onready var quit_button = $PanelContainer/VBoxContainer/HBoxContainer/Quit
 
@@ -20,6 +22,7 @@ func _ready():
 	update_stats()
 	resume_button.connect("pressed", _on_close_pressed)
 	visible = false # Start hidden
+	identify_button_list.visible = false
 
 func _input(event):
 	if event is InputEventKey and event.pressed and event.keycode in [KEY_TAB, KEY_ESCAPE]:
@@ -43,26 +46,40 @@ func update_stats():
 	update_inventory()
 
 func update_inventory():
-	for child in inventory_list.get_children():
-		child.queue_free()
+	for column in inventory_list.get_children():
+		for child in column.get_children():
+			child.queue_free()
 	
 	for item in player.inventory_node.get_children():
 		var use_button = Button.new()
+		var identify_button = Button.new()
 		var item_known_as = player.known(item.entity_name)
 		if (item.stackable):
 			use_button.text = "Use %s (Currently have x%d)" % [item_known_as, item.count]
 		else:
 			use_button.text = "Use %s" % item_known_as
+		identify_button.text = "Identify %s" % item_known_as
 		use_button.pressed.connect(_on_use_pressed.bind(use_button))
-		inventory_list.add_child(use_button)
+		use_button_list.add_child(use_button)
+		identify_button.pressed.connect(_on_identify_pressed.bind(identify_button))
+		identify_button_list.add_child(identify_button)
 
 func _on_close_pressed():
 	visible = false
 	GameMaster.can_move = true
 
-func _on_use_pressed(useButton: BaseButton):
-	var index = useButton.get_index()
+func enable_identify():
+	identify_button_list.visible = true
+
+func _on_use_pressed(use_button: BaseButton):
+	var index = use_button.get_index()
 	player.use(index)
+	update_stats()
+
+func _on_identify_pressed(identify_button: BaseButton):
+	var index = identify_button.get_index()
+	player.identify(index)
+	identify_button_list.visible = false
 	update_stats()
 
 func _on_quit_pressed():
