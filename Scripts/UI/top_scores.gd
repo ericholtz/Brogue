61c5e15630b798bd6daea0cf5139130f4bcedcab
AlexanderTCSE <tattersallalexd@gmail.com>
2025-04-23 12:51:35 -0700

    fix: score back button sfx

--- Scripts/UI/top_scores.gd
+++ Scripts/UI/top_scores.gd
@@ -27,4 +27,5 @@ func load_top_scores():
 		scores_label.text = "No scores file found."
 
 func _on_back_pressed():
+	SoundFx.menu_no()
 	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
